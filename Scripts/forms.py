4efb59556aed4cd59ecf29117535a2bf5454c7df
Palash Ahmed <49250151+palahsu@users.noreply.github.com>
2021-03-25 23:29:29 +0600

    Update forms.py

--- Scripts/forms.py
+++ Scripts/forms.py
@@ -4,5 +4,5 @@ from wtforms import Form, StringField, validators
 
 class LoginForm(Form):
     username = StringField('Username:', validators=[validators.required(), validators.Length(min=1, max=30)])
-    password = StringField('Password:', validators=[validators.required(), validators.Length(min=1, max=30)])
+    password = StringField('Password:', validators=[validators.required(), validators.Length(min=1, max=50)])
     email = StringField('Email:', validators=[validators.optional(), validators.Length(min=0, max=50)])
